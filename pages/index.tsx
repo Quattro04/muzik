9c7a3acc2256c85ce0a81f16e92789ba99a0aea8
Matija Jeras <matija.jeras64@gmail.com>
2023-04-15 11:54:43 +0200

    mobile design

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,16 @@ import { FormEvent, useEffect, useState } from 'react'
 import Player from '@/components/Player'
 import Layout from '@/components/Layout'
 import { Song, useSongs } from '@/context/SongsContext'
-import BarsAnimaiton from '@/components/BarsAnimation'
-import { useRouter } from 'next/router'
 import LoadingSpinner from '@/components/LoadingSpinner'
 import { useInfo } from '@/context/InfoContext'
 import { useUser } from '@/context/UserContext'
 
 export default function Home() {
 
-    const [isLoading, setLoading] = useState(true)
-    const [songLoading, setSongLoading] = useState<number>(-1);
+    const [isLoading, setIsLoading] = useState(true)
+    const [songLoading, setSongLoading] = useState<string>('');
     const [username, setUsername] = useState<string>('');
-    
-    const router = useRouter();
+
     const { songs, playedSong, isPlaying, fetchSongs, playSong } = useSongs();
     const { isMobile } = useInfo();
     const { user, setUser } = useUser();
@@ -24,17 +21,15 @@ export default function Home() {
 
     useEffect(() => {
         fetchSongs(() => {
-            setLoading(false)
+            setIsLoading(false)
         });
     }, [])
 
     const onPlaySong = (song: Song) => {
-        playSong(song);
-    }
-
-    const getIndex = (song: Song | undefined) => {
-        if (!song || !user) return -1;
-        return songs.filter(s => s.users.includes(user)).findIndex(s => s === song);
+        setSongLoading(song.id)
+        playSong(song, () => {
+            setSongLoading('');
+        });
     }
 
     const usernameSubmit = (e: FormEvent) => {
@@ -53,25 +48,28 @@ export default function Home() {
             </Head>
             <Layout>
                 {user &&
-                    <ul className="flex w-full flex-col mb-auto divide-y divide-lightblue mx-auto max-w-screen-xl">
+                    <ul className="flex w-full flex-col mb-auto mx-auto max-w-screen-xl">
                         {songs.filter(s => s.users.includes(user)).map((song, idx) =>
                             <li
                                 key={idx}
-                                className={`relative basis-12 flex items-center flex-1 pr-4 cursor-pointer rounded ${playedSong?.id === song.id ? 'bg-lightblue' : ''}`}
-                                style={{ paddingLeft: songLoading === idx ? (isMobile ? '71px' : '87px') : '0' }}
+                                className={`relative basis-12 flex items-center flex-1 py-2 sm:py-3 px-3 sm:px-6 cursor-pointer rounded ${songLoading === song.id || playedSong?.id === song.id ? 'bg-lightblue' : ''}`}
                                 onClick={() => onPlaySong(song)}
                             >
-                                {songLoading !== idx && (getIndex(playedSong) !== idx || !isPlaying) &&
-                                    <img className="mr-4 sm:mr-8 rounded" src={song.image} width={55} height={40} alt="Art cover" />
+                                {songLoading !== song.id &&
+                                    <div className="w-10 h-10 sm:w-12 sm:h-12 relative mr-4">
+                                        <img className="h-full rounded" src={song.image} alt="Art cover" style={{ objectFit: 'cover' }} />
+                                    </div>
                                 }
-                                <span className="text-white flex-1 text-xs sm:text-sm mr-4 pointer-events-none">{song.title}</span>
-                                <span className="text-white flex-1 text-xs sm:text-sm opacity-80 mr-4 pointer-events-none">{song.artist}</span>
-                                <span className="text-white text-xs sm:text-sm opacity-80 pointer-events-none" style={{ flexBasis: '40px' }}>{song.releaseYear}</span>
-                                {songLoading === idx &&
-                                    <div className="flex items-center absolute left-0 top-0 w-full h-full pl-3">
+                                {songLoading === song.id &&
+                                    <div className="w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center mr-4">
                                         <LoadingSpinner width={30} height={30} />
                                     </div>
                                 }
+                                <div className="flex flex-1 flex-col sm:flex-row overflow-hidden">
+                                    <span className="text-white flex-1 text-xs sm:text-sm mr-4 pointer-events-none truncate">{song.title}</span>
+                                    <span className="text-white flex-1 text-xs sm:text-sm opacity-50 mr-4 pointer-events-none truncate">{song.artist}</span>
+                                </div>
+                                <span className="text-white text-xs sm:text-sm opacity-80 pointer-events-none" style={{ flexBasis: '40px' }}>{song.releaseYear}</span>
                             </li>
                         )}
                     </ul>
