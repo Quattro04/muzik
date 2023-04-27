04ecc4bc07691160dc12661bab04a5edd899de92
Matija Jeras <matija.jeras64@gmail.com>
2023-04-27 09:52:18 +0200

    header set, downgrade ytdl

--- pages/api/audio/[ytid].ts
+++ pages/api/audio/[ytid].ts
@@ -3,8 +3,18 @@ import ytdl from 'ytdl-core';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { ytid } = req.query
-    const url = `https://www.youtube.com/watch?v=${ytid}`
-    const videoInfo = await ytdl.getInfo(url);
-    const audioFormat = ytdl.chooseFormat(videoInfo.formats, { filter: 'audioonly' });
-    ytdl(url, { format: audioFormat }).pipe(res);
+
+    try {
+        const url = `https://www.youtube.com/watch?v=${ytid}`
+        const videoInfo = await ytdl.getInfo(url);
+        const audioFormat = ytdl.chooseFormat(videoInfo.formats, { filter: 'audioonly' });
+
+        res.setHeader('Content-Length', audioFormat.contentLength);
+        res.setHeader('Content-Type', 'audio/mpeg');
+        res.setHeader('Content-Disposition', `attachment; filename="${videoInfo.videoDetails.videoId}.mp3"`);
+
+        ytdl(url, { format: audioFormat }).pipe(res);
+    } catch (e) {
+        console.log('ERROR YTDL: ', e);
+    }
 }
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,18 +1,37 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Text() {
 
     const [audioUrl, setAudioUrl] = useState('');
+    const [audioUrl2, setAudioUrl2] = useState('');
+    const [playerId, setPlayerId] = useState<number>(0);
 
-    const play = async () => {
+    const audioRef1 = useRef<HTMLVideoElement>(null);
+    const audioRef2 = useRef<HTMLVideoElement>(null);
 
+    const next = async () => {
+        if (audioRef1.current) {
+            audioRef1.current.pause();
+        }
+        setPlayerId(1);
+        if (audioRef2.current) {
+            audioRef2.current.play();
+        }
     }
 
     useEffect(() => {
-        play();
+        setAudioUrl('/api/audio/4e_tXnbuv7w')
+        setTimeout(() => {
+            setAudioUrl2('/api/audio/tp4fUH2E8uc')
+        }, 2000)
     }, [])
 
     return (
-        audioUrl && <audio src={audioUrl} controls autoPlay />
+        <div>
+            <audio src={audioUrl} controls={playerId === 0} ref={audioRef1} />
+            <audio src={audioUrl2} controls={playerId === 1} ref={audioRef2} />
+            <button onClick={() => next()}>SET 2</button>
+            <iframe src="https://open.spotify.com/embed/track/1Sl3njkhhz8nrSPZroDQ82?utm_source=generator" width="100%" height="152" frameBorder="0" allowFullScreen={false} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </div>
     )
 }
